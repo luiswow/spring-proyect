51c4b861dbf818baad13e37d84de80e700023ded
user <user@users-MacBook-Pro.local>
2020-03-28 13:03:20 -0600

    Inicio del Proyecto

---
+++ src/main/java/com/chivisgt/SpringDataJpasGitApplication.java
@@ -0,0 +1,13 @@
+package com.chivisgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpasGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpasGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chivisgt/SpringDataJpasGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chivisgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpasGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
